8fec3278be015ee9e92cff346b042a72db92d469
Aearsears <53102185+Aearsears@users.noreply.github.com>
2024-11-22 16:00:03 -0500

    pipe output to cli pager

--- ascii.py
+++ ascii.py
@@ -1,12 +1,36 @@
+import subprocess
+import os
+import sys
 from PIL import Image
 
 CHARACTERS = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]
 
+is_windows = sys.platform == 'win32'
+
+is_macos = sys.platform == 'darwin'
+
+
+if is_windows:
+    default_pager = 'more'
+    eol = "\r\n"
+else:
+    default_pager = 'less -R'
+    eol = "\n"
+
+# update pager based on platform
+process = subprocess.Popen(
+    ['less', '-R'], stdin=subprocess.PIPE, text=True
+)
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def img2ascii():
     im = Image.open("pic.jpg")
-    width = 150
-    height = 50
+    width = 10
+    height = 10
     grayscale = im.convert("L").resize((width, height))
     ascii = []
     line = ""
@@ -14,11 +38,13 @@ def img2ascii():
         index = int((255-pixel)*10/256)
         line += CHARACTERS[index]
         if len(line) == width:
+            line += eol
             ascii.append(line)
             line = ""
-    for l in ascii:
-        print(l)
+
+    process.communicate(input="".join(ascii))
 
 
 if __name__ == "__main__":
+    cls()
     img2ascii()
